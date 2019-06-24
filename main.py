cf11644d67881d7c6fdb9916ee0779d4b6e59d36
eviau <github@eviau.net>
2019-06-24 16:02:50 -0400

    comments partie 1:done

--- main.py
+++ main.py
@@ -9,20 +9,27 @@ from src import luminx
 import argparse
 
 if __name__ == "__main__":
-
-    # ici tu peux utiliser argparse pour lire les arguments: https://docs.python.org/3/library/argparse.html
-
-    parser = argparse.ArgumentParser(description='Import financial data and generate a LED program.')
+    parser = argparse.ArgumentParser(
+        description='Import financial data and generate a LED program.')
 
     parser.add_argument('--fromweb',
-                    action="store_true")
+                        action="store_true",
+                        help="If you want to import new data from the web.")
     parser.add_argument('--fromcsv',
-                    action="store_true")
+                        action="store_true",
+                        help="If you want to use data already imported in a CSV.")
     parser.add_argument('--yesgif',
-                    action="store_true")
-    parser.add_argument('-s', type=str, dest='start_day')
-    parser.add_argument('-e', type=str, dest='end_day')
-    parser.add_argument('-n',type=str, dest='name_movie')
+                        action="store_true",
+                        help="If you want to generate a gif with the data.")
+    parser.add_argument('-s', type=str,
+                        dest='start_day',
+                        help="The first day of the imported time series.")
+    parser.add_argument('-e', type=str,
+                        dest='end_day',
+                        help="The last day of the imported time series.")
+    parser.add_argument('-n', type=str,
+                        dest='name_movie',
+                        help="The name of the gif that will be generated.")
 
     args = parser.parse_args()
 
@@ -30,14 +37,12 @@ if __name__ == "__main__":
         pure_data = data_preprocess.import_from_the_web(
             args.start_day, args.end_day)
     elif args.fromcsv:
-        pure_data = data_preprocess.import_from_csv(args.start_day, args.end_day)
+        pure_data = data_preprocess.import_from_csv(
+            args.start_day, args.end_day)
     else:
-        print('Valid modes are: -fromweb and -fromcsv. Try again !')
-        # bug ici: il faut quitter la fonction! 
-        # tu peux utiliser sys.exit pour quitter le script entierement
-        # la convention sous Unix est de retourner un code d'erreur different de 0 quand le programme
-        # s'arrete avec une erreur, et 0 si tout va bien. Python va retourner 0 automatiquement.
-        # https://docs.python.org/3/library/argparse.html
+        print('Valid modes are: --fromweb and --fromcsv. Try again !')
+        sys.exit("Error: No valid mode was given.")
+
     if args.yesgif:
         (red_df, blue_df) = luminx.from_formatted_to_colours(pure_data)
         luminx.from_colours_to_screen(
