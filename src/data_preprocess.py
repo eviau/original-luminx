81f2442b22bde20cb3481d87a89db7e46cbf20c9
Adrien Lamarque <ad@lamarque.fr>
2019-06-25 16:49:25 -0400

    Update data_preprocess.py

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -7,7 +7,11 @@ from pandas.io.json import json_normalize
 
 import json
 
-
+# import_from_the_web est censé retourner une valeur?
+# suggestion: soit faire en sorte qu'import_from_the_web recuperer les donnees et l ecrive sous un ficher
+# en plus de la retourner, soit faire qu'import_from_the_web ne la retourne pas, et appeler
+# inconditionellement import_from_csv apres avoir appele import_from_the_web (probablement apres avoir renomme 
+# import_from_the_web en fetch_data ou qqch comme ca)
 def import_from_the_web(start_date, end_date):
     r = requests.get(
         'https://api.exchangeratesapi.io/history?start_at=' + start_date + '&end_at=' + end_date)
