2f4863eecf9106f0e2a8cef694d90cc16467f73f
eviau <github@eviau.net>
2019-06-25 17:26:42 -0400

    added my answers

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -1,11 +1,8 @@
-import ast
-
-import requests
-import pandas as pd
-
-from pandas.io.json import json_normalize
-
 import json
+import pandas as pd
+from pandas.io.json import json_normalize
+import requests
+
 
 def import_from_the_web(start_date, end_date):
     r = requests.get(
@@ -15,6 +12,7 @@ def import_from_the_web(start_date, end_date):
     df.to_csv('./data/from' + start_date +
               '_to_' + end_date + '_formatted.csv')
     return df
+
 
 def import_from_csv(start_date, end_date):
     return pd.read_csv('./data/from' + start_date +
--- src/luminx.py
+++ src/luminx.py
@@ -1,9 +1,9 @@
+import imageio
 import pandas as pd
-
 from PIL import Image
 import numpy as np
 
-import imageio
+NB_CHANNELS_RGB = 3
 
 
 def translate_format_range(x, input_low, input_high, output_low, output_high):
@@ -18,24 +18,27 @@ def from_formatted_to_colours(df):
     df.fillna(0, inplace=True)
     df = df.sort_index().pct_change()
     df.fillna(0, inplace=True)
-    # sauvegarder df < 0 et df >= 0 dans une variable intermediaire
+    to_be_red = df[df < 0]
+    to_be_blue = df[df >= 0]
     red_df = translate_format_range(
-        df[df < 0], df[df < 0].min(), df[df < 0].max(), 0.0, 255.0)
+        to_be_red, to_be_red.min(), to_be_red.max(), 0.0, 255.0)
     blue_df = translate_format_range(
-        df[df >= 0], df[df >= 0].min(), df[df >= 0].max(), 0.0, 255.0)
+        to_be_blue, to_be_blue.min(), to_be_blue.max(), 0.0, 255.0)
     return (red_df, blue_df)
 
 
 def from_colours_to_screen(red_df, blue_df, name, size_led, space_led):
     # pourquoi space * 2?
+    # - parce que space_led est l'espace entourant chaque led, et on le double, de façon arbitraire
     width_strip = red_df.columns.shape[0]*(space_led*2+size_led)
     height_strip = size_led + space_led*2
-    # pourquoi 3 ?
-    data = np.zeros((height_strip, width_strip, 3), dtype=np.uint8)
+    data = np.zeros((height_strip, width_strip,
+                     NB_CHANNELS_RGB), dtype=np.uint8)
     n = space_led
     frames = []
-    
+
     # que represente n?
+    # n représente le nouveau point de départ de la led suivante, c'est l'axe horizontal (les colonnes du np.array vide)
     for m in range(0, red_df.shape[0]):
         for col in red_df.columns:
             data[space_led:(size_led+space_led), n:(n+size_led)
