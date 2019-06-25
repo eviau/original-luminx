c2e070551effe40251b2a94587b85c818808de0f
Adrien Lamarque <ad@lamarque.fr>
2019-06-25 16:46:14 -0400

    Update luminx.py

--- src/luminx.py
+++ src/luminx.py
@@ -18,6 +18,7 @@ def from_formatted_to_colours(df):
     df.fillna(0, inplace=True)
     df = df.sort_index().pct_change()
     df.fillna(0, inplace=True)
+    # sauvegarder df < 0 et df >= 0 dans une variable intermediaire
     red_df = translate_format_range(
         df[df < 0], df[df < 0].min(), df[df < 0].max(), 0.0, 255.0)
     blue_df = translate_format_range(
@@ -26,11 +27,15 @@ def from_formatted_to_colours(df):
 
 
 def from_colours_to_screen(red_df, blue_df, name, size_led, space_led):
+    # pourquoi space * 2?
     width_strip = red_df.columns.shape[0]*(space_led*2+size_led)
     height_strip = size_led + space_led*2
+    # pourquoi 3 ?
     data = np.zeros((height_strip, width_strip, 3), dtype=np.uint8)
     n = space_led
     frames = []
+    
+    # que represente n?
     for m in range(0, red_df.shape[0]):
         for col in red_df.columns:
             data[space_led:(size_led+space_led), n:(n+size_led)
